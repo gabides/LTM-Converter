0f394f0e46422b3a27d02f08803c37e57fe1d7d2
gabides <gdittrick@gmail.com>
2018-05-15 13:20:19 +0200

    Initial Commit

---
+++ LTM-Converter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LTM-Converter
+//
+//  Created by Gabriel Dittrick on 15/05/2018.
+//  Copyright © 2018 LTM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
